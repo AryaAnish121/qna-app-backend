f0db90f188ecb9697d2f55d16957df1ba1a533ee
AryaAnish121 <anishkumarsarmi@gmail.com>
2024-08-04 23:43:05 +0530

    Send file without creating file in fs

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import createDocx from "./createDocx.js";
 import { Packer } from "docx";
-import fs from "fs";
-import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,10 +16,12 @@ app.get("/", (req, res) => {
 app.post("/generate", async (req, res) => {
   const doc = createDocx(req.body);
   const buffer = await Packer.toBuffer(doc);
-  const filePath = `out/${uuidv4()}.docx`;
-  if (!fs.existsSync("out")) fs.mkdirSync("out");
-  fs.writeFileSync(filePath, buffer);
-  res.download(filePath);
+  res.setHeader("Content-Disposition", `attachment; filename="document.docx"`);
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  );
+  res.send(buffer);
 });
 
 app.listen(PORT, function () {
